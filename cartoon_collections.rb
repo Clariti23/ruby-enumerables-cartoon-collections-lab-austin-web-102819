987eb2576c8957bd61d3ab0a3de90cc38ca8580d
Grant Ferowich <g.ferowich@gmail.com>
2019-10-26 18:53:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,8 @@ def long_planeteer_calls# code an argument here
   # Your code here
 end
 
+def long_planeteer_calls
+
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
